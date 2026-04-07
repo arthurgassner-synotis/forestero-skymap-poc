b281f3edadc41e707b9074024254bdc735731163
Arthur Gassner <arthur.gassner@synotis.ch>
2026-04-07 15:12:44 +0200

    feat: plot first Sentinel img

--- nb/constants.py
+++ nb/constants.py
@@ -6,3 +6,6 @@ BRONZE_FOLDERPATH = DATA_FOLDERPATH / "bronze"
 
 # Ghana tree coords
 GH_TREE_COORDS_FILEPATH = BRONZE_FOLDERPATH / "GH Tree coordinates.xlsx"
+
+# Sentinel scene
+SENTINEL_SCENES_FOLDERPATH = BRONZE_FOLDERPATH / "sentinel-scenes"
